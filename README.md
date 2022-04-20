d6aa3ec57218ee0e628946864a325e0f6619db0f
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-04-20 02:18:17 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+
+# PãoBot
+
+Meu primeiro projeto em python, um bot para [Discord](https://discord.com/) feito inicialmente com a biblioteca [Discord.py](https://github.com/Rapptz/discord.py), que foi descontinuada, sendo agora transferido para Disnake.
+
+O prefixo do bot é `p.`.
+
---
+++ main.py
@@ -0,0 +1,66 @@
+import disnake
+from disnake.ext import commands, tasks
+import json
+import os 
+from datetime import datetime 
+import pytz
+import asyncio
+
+
+# Get configuration.json
+with open("configuration.json", "r") as config: 
+  data = json.load(config)
+  token = data["token"]
+  prefix = data["prefix"]
+
+
+# Intents
+intents = disnake.Intents.default()
+# The bot
+bot = commands.Bot(prefix, intents = intents, case_insensitive = True)
+
+tz_BR = pytz.timezone('America/Sao_Paulo') 
+datetime_BR = datetime.now(tz_BR)
+
+@bot.event
+async def on_ready():
+  bot.load_extension('cogs.')
+
+for filename in os.listdir('./Cogs'):
+  if filename.endswith('.py'):
+    bot.load_extension(f'Cogs.{filename[:-3]}')
+
+@bot.event
+async def on_ready():
+
+
+  print ('===============================')
+  print (f'Acordei pra tomar café às {(datetime_BR.strftime("%H:%M"))}')
+
+
+  #ligado.start()
+  
+  bot.loop.create_task(status_task())
+  #ligado.stop()
+  del bot.on_ready
+
+#@tasks.loop(seconds=1)
+#async def ligado():
+  user = [319963626108878848]
+  for id in user:
+    member = await bot.fetch_user(id)
+    try:
+      await member.send(f'{member.mention} Acordei pra tomar café às {(datetime_BR.strftime("%H:%M"))}')
+    except:
+      pass
+
+
+async def status_task():
+    while True:
+        await bot.change_presence(activity=disnake.Activity(type=disnake.ActivityType.watching,
+                name=f'Digite {prefix}help | Estou em {len(bot.guilds)} servidores'),status=disnake.Status.online)
+        await asyncio.sleep(1800)
+
+
+
+bot.run(token)
